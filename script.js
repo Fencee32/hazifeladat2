30049c46bbdeda3c9f14957063dcce9a93bd2bc1
Bence <bence00504@gmail.com>
2023-09-11 08:38:48 +0200

    kuksi

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log('script loaded')
-console.error('asd')
+const container = document.querySelector('container')
+document.getElementById('uzenet').onclick = function () {
+  alert('Boldog új évet!')
+}
+console.log('cső')
+console.error('nem')
 
-document.querySelector('h1')
-h1Element.addEventListener
+document.querySelector('h1').onclick = function () {
+  alert('Boldog új évet!')
+}
+
+document.querySelector('h2').onclick = function () {
+  alert('Boldog új évet!')
+}
+
+const uzenet = document.getElementById('uzenet')
+uzenet.style.cursor = 'pointer'
