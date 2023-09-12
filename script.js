e97b829db28b6d826d1f656f1306de71283f2efc
Your Name <bence00504@gmail.com>
2023-09-12 08:41:08 +0200

    commitolom

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 document.getElementById('uzenet').onclick = function () {
   alert('Szervbusz')
-  }
-  console.log('cső')
-  console.error('nem')
-  
-  
-  document.querySelector('h2').onclick = function () {
-  alert('Boldog új évet!')
-  }
-  
-document.getElementById('dobas').onclick = function () {
-const container = document.getElementById("container");
-const cardValue = Math.floor(Math.random() * 6) + 1;
-const card = document.createElement("div");
-card.textContent = cardValue;
-container.appendChild(card);
+}
+console.log('cső')
+console.error('nem')
 
+document.querySelector('h2').onclick = function () {
+  alert('Boldog új évet!')
 }
 
+document.getElementById('dobas').onclick = function () {
+  const container = document.getElementById('container')
+  let card = document.createElement('div')
+  card.classList.add('card')
+  const cardValue = Math.floor(Math.random() * 6) + 1
+  card.textContent = cardValue
+  container.append(card)
+}
 
 const uzenet = document.getElementById('uzenet')
 uzenet.style.cursor = 'pointer'
